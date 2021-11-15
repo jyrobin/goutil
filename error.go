4016469e93a483f709d757efa2c88eec4d7aad45
Jing-Ying Chen <jyc@cs.nctu.edu.tw>
2021-11-15 23:04:24 +0800

    add BufferError

---
+++ error.go
@@ -0,0 +1,46 @@
+package goutil
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+// BufferError is not really of an error type but to "tunnel" return data
+// from functions that only return errors
+type BufferError interface {
+	error
+	io.ReadWriter
+	Len() int
+	Bytes() []byte
+	String() string
+}
+
+type BytesBufferError struct {
+	bytes.Buffer
+	msg string
+}
+
+func NewBufferError(msg string, data []byte) *BytesBufferError {
+	return &BytesBufferError{
+		Buffer: *bytes.NewBuffer(append([]byte(nil), data...)),
+		msg:    msg,
+	}
+}
+
+func NewStringError(msg, data string) *BytesBufferError {
+	return &BytesBufferError{
+		Buffer: *bytes.NewBufferString(data),
+		msg:    msg,
+	}
+}
+
+func NewJsonError(msg string, data interface{}) *BytesBufferError {
+	buf, err := json.Marshal(data)
+	if err != nil {
+		return NewBufferError(err.Error(), nil)
+	}
+	return NewBufferError(msg, buf)
+}
+
+func (e BytesBufferError) Error() string { return e.msg }

84ef4b7200a3970b3c1ace534c9c0df9a5351657
Jing-Ying Chen <jyc@cs.nctu.edu.tw>
2022-01-26 13:58:52 +0800

    Remove KVs

--- context.go
+++ context.go
@@ -15,13 +15,3 @@ func ContextWithMap(ctx context.Context, maps ...map[string]interface{}) context
 	}
 	return ctx
 }
-
-func ContextWithKVs(ctx context.Context, kvs ...KV) context.Context {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	for _, kv := range kvs {
-		ctx = context.WithValue(ctx, kv.Key, kv.Value)
-	}
-	return ctx
-}
